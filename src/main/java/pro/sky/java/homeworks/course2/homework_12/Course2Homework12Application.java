39e7c3341e2885fe6f37e68cc5b6526807969ca1
andrehsym <kochetkovandre1993@gmail.com>
2022-01-16 17:08:48 +0300

    first commit

---
+++ src/main/java/pro/sky/java/homeworks/course2/homework_12/Course2Homework12Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.homeworks.course2.homework_12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Homework12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2Homework12Application.class, args);
+    }
+
+}
